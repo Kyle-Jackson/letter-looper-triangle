6f7780d9ba2d3f60481108556e62abb3fb26782c
Kyle-Jackson <kylejackson@clare-pc.gateway.2wire.net>
2016-03-03 08:44:29 -0500

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var letter = prompt('Enter a letter:');
+
+// The `i` variable is a counter, to count how many times the loop has occurred
+var i;
+
+for (i = 0; i < 11; i++) {
+  document.write(letter + '<br>');
+}
+
+// // Let’s create a loop that repeats 5 times
+// for (i = 0; i < 10; i++) {
+//   // The code in here will be executed 5 times
+// }
